10ef3d70fcaec015e7ef21a78b93a5e1493a3c17
vauxhall <zero.griffin@gmail.com>
2020-11-13 13:01:36 +0100

    Make color be part of bead

---
+++ Sources/Bead.Color.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+extension Bead {
+    public enum Color: String, Codable, CaseIterable {
+        case
+        blue,
+        pink,
+        red,
+        orange,
+        green,
+        yellow,
+        teal,
+        purple,
+        indigo
+    }
+}
