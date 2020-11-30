b7c1e0592f278f383122600ead30c267d7ea11c6
vaux <zero.griffin@gmail.com>
2020-11-30 12:02:57 +0100

    Update states

--- Sources/Match.State.swift
+++ Sources/Match.State.swift
@@ -1,24 +1,43 @@
 import Foundation
 
 extension Match {
-    public enum State: UInt8, Codable {
+    public enum State: Decodable {
         case
         new,
         matching,
-        first,
-        second,
-        prizeRobot,
-        prizeFirst,
-        prizeSecond,
-        remove,
-        end
+        cancel,
+        play(Turn),
+        timeout(Turn),
+        win(Turn),
+        end(Bead)
         
-        var next: Self {
-            switch self {
-            case .first: return .second
-            case .second: return .first
-            default: return [.first, .second].randomElement()!
-            }
+        public init(from decoder: Decoder) throws {
+            self = {
+                (try? $0.decodeIfPresent(Key.self, forKey: .matching)).map { _ in
+                    .matching
+                } ?? (try? $0.decodeIfPresent(Key.self, forKey: .cancel)).map { _ in
+                    .cancel
+                } ?? (try? $0.decodeIfPresent(Turn.self, forKey: .play)).map {
+                    .play($0)
+//                } ?? (try? $0.decodeIfPresent(Turn.self, forKey: .timeout)).map {
+//                    .timeout($0)
+                } ?? (try? $0.decodeIfPresent(Turn.self, forKey: .win)).map {
+                    .win($0)
+                } ?? (try? $0.decodeIfPresent(Bead.self, forKey: .end)).map {
+                    .end($0)
+                }
+            } (try decoder.container(keyedBy: Key.self)) ?? .new
+        }
+        
+        private enum Key: String, Codable, CodingKey {
+            case
+            new,
+            matching,
+            cancel,
+            play,
+            timeout,
+            win,
+            end
         }
     }
 }
