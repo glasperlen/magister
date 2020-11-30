0b7d5dc63da143dd46004f79f4311b9daa97da9a
vaux <zero.griffin@gmail.com>
2020-11-30 12:52:20 +0100

    Update state

--- Sources/Match.State.swift
+++ Sources/Match.State.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Match {
-    public enum State: Decodable {
+    public enum State: Codable {
         case
         new,
         matching,
@@ -11,22 +11,41 @@ extension Match {
         win(Turn),
         end(Bead)
         
-        public init(from decoder: Decoder) throws {
-            self = {
-                (try? $0.decodeIfPresent(Key.self, forKey: .matching)).map { _ in
-                    .matching
-                } ?? (try? $0.decodeIfPresent(Key.self, forKey: .cancel)).map { _ in
-                    .cancel
-                } ?? (try? $0.decodeIfPresent(Turn.self, forKey: .play)).map {
-                    .play($0)
-//                } ?? (try? $0.decodeIfPresent(Turn.self, forKey: .timeout)).map {
-//                    .timeout($0)
-                } ?? (try? $0.decodeIfPresent(Turn.self, forKey: .win)).map {
-                    .win($0)
-                } ?? (try? $0.decodeIfPresent(Bead.self, forKey: .end)).map {
-                    .end($0)
+        public init(from: Decoder) throws {
+            self = try {
+                if try $0.decodeIfPresent(Key.self, forKey: .matching) != nil {
+                    return .matching
                 }
-            } (try decoder.container(keyedBy: Key.self)) ?? .new
+                if try $0.decodeIfPresent(Key.self, forKey: .cancel) != nil {
+                    return .cancel
+                }
+                if let turn = try $0.decodeIfPresent(Turn.self, forKey: .play) {
+                    return .play(turn)
+                }
+                if let turn = try $0.decodeIfPresent(Turn.self, forKey: .timeout) {
+                    return .timeout(turn)
+                }
+                if let turn = try $0.decodeIfPresent(Turn.self, forKey: .win) {
+                    return .win(turn)
+                }
+                if let bead = try $0.decodeIfPresent(Bead.self, forKey: .end) {
+                    return .end(bead)
+                }
+                return .new
+            } (from.container(keyedBy: Key.self))
+        }
+        
+        public func encode(to: Encoder) throws {
+            var container = to.container(keyedBy: Key.self)
+            switch self {
+            case .new: try container.encode(Key.new, forKey: .new)
+            case .matching: try container.encode(Key.matching, forKey: .matching)
+            case .cancel: try container.encode(Key.cancel, forKey: .cancel)
+            case let .play(turn): try container.encode(turn, forKey: .play)
+            case let .timeout(turn): try container.encode(turn, forKey: .timeout)
+            case let .win(turn): try container.encode(turn, forKey: .win)
+            case let .end(bead): try container.encode(bead, forKey: .end)
+            }
         }
         
         private enum Key: String, Codable, CodingKey {
