96a48493743c99bae50d5f557a09a1ad24bf79aa
vaux <zero.griffin@gmail.com>
2021-01-19 11:50:27 +0100

    Make bead identifiable

--- Sources/Bead.swift
+++ Sources/Bead.swift
@@ -1,7 +1,8 @@
 import Foundation
 
-public struct Bead: Codable, Hashable {
+public struct Bead: Codable, Identifiable, Hashable {
     public var tier: Int { self[.top] + self[.bottom] + self[.left] + self[.right] }
+    public let id: UUID
     public let color: Color
     private let relations: [Relation : Int]
     
@@ -27,13 +28,18 @@ public struct Bead: Codable, Hashable {
     public init(color: Color = Color.allCases.randomElement()!, top: Int = 0, bottom: Int = 0, left: Int = 0, right: Int = 0) {
         self.color = color
         relations = [.top: top, .bottom: bottom, .left: left, .right: right]
+        id = .init()
     }
     
     public subscript(_ relation: Relation) -> Int {
         relations[relation]!
     }
     
-//    public static func == (lhs: Self, rhs: Self) -> Bool {
-//        lhs.color == rhs.color && lhs.relations == rhs.relations
-//    }
+    public func hash(into: inout Hasher) {
+        into.combine(id)
+    }
+    
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.id == rhs.id
+    }
 }
